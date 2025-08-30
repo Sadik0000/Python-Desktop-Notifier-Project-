6d54fa47b7183904b99ab1e954078967d5d07d66
Sadik0000 <sadikahmedtanjim@gmail.com>
2025-08-30 20:20:25 +0600

    Update Windows Desktop Notification App.py

--- Windows Desktop Notification App.py
+++ Windows Desktop Notification App.py
@@ -1,6 +1,6 @@
 from win10toast import ToastNotifier #external module
 toaster  = ToastNotifier()
 
-toaster.show_toast("Learning Time", "Hello Eshan It's Time To Learn", duration=5)
+toaster.show_toast("Learning Time", "Hello Tanjim It's Time To Learn", duration=5)
 
-toaster.show_toast("Gaming Play Time", "Hello Eshan It's Time To Play New game", duration=10)
+toaster.show_toast("Gaming Play Time", "Hello Tanjim It's Time To Play New game", duration=10)
